9611aff0252338d70f24a8444a2c85619e2751e4
Bill Prin <waprin@google.com>
2015-09-14 11:31:41 -0700

    fix local test

--- test_main.py
+++ test_main.py
@@ -22,8 +22,8 @@ class MainTest(unittest.TestCase):
         self.app = main.app.test_client()
 
     def test_hello_world(self):
-        rv = self.app.get('/')
-        assert rv.data == 'Hello world'
+        rv = self.app.get('/get_author/ulysses')
+        assert rv.data == 'James Joyce'
 
 if __name__ == '__main__':
     unittest.main()
